8fe956adfc5cb03ad39042fa3e3497c42d7fbce1
Justin Richer <justin@bspk.io>
2021-12-10 16:39:17 -0500

    updated build path and syntax for unicode inclusion

--- build.sh
+++ build.sh
@@ -3,6 +3,5 @@
 mkdir -p publish
 cp _redirects publish/_redirects
 kramdown-rfc2629 -3 draft-ietf-gnap-core-protocol.md > publish/draft-ietf-gnap-core-protocol.xml
-xml2rfc --v2v3 publish/draft-ietf-gnap-core-protocol.xml -o publish/draft-ietf-gnap-core-protocol.xml
-xml2rfc --text publish/draft-ietf-gnap-core-protocol.xml
-xml2rfc --html publish/draft-ietf-gnap-core-protocol.xml
+xml2rfc --v3 --text publish/draft-ietf-gnap-core-protocol.xml
+xml2rfc --v3 --html publish/draft-ietf-gnap-core-protocol.xml
